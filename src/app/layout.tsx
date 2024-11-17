2c91325bde30b59bf7129e037f920e3b4d4d28be
oscarhernandeziv <oscarhernandeziv@gmail.com>
2024-11-16 21:28:49 -0800

    Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
+import { SessionProvider } from "@/components/SessionProvider";
+import UserButton from "@/components/UserButton";
 
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
@@ -20,21 +22,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistMono.className} px-2 md:px-5`}>
-        <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
-          <div className="flex flex-grow">
-            <Link href="/">Next Chat App</Link>
-            <Link href="/about" className="ml-5 font-light">
-              About
-            </Link>
+    <SessionProvider>
+      <html lang="en">
+        <body className={`${geistMono.className} px-2 md:px-5`}>
+          <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+            <div className="flex flex-grow">
+              <Link href="/">Next Chat App</Link>
+              <Link href="/about" className="ml-5 font-light">
+                About
+              </Link>
+            </div>
+            <div>
+              <UserButton />
+            </div>
+          </header>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-grow">{children}</div>
           </div>
-          <div></div>
-        </header>
-        <div className="flex flex-col md:flex-row">
-          <div className="flex-grow">{children}</div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
